da49d9a5fe9d378b57e0a99080cb28ddd30f1691
Kazuki Ota <kaota@microsoft.com>
2019-10-14 11:55:42 +0900

    Add project files.

---
+++ ComboBoxSample/Item.cs
@@ -0,0 +1,37 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace ComboBoxSample
+{
+    public class Item
+    {
+        public ReadOnlyReactivePropertySlim<string> Name { get; }
+
+        public Item(string name)
+        {
+            Name = Observable.Interval(TimeSpan.FromSeconds(10))
+                .Select(_ => $"{name}: {Guid.NewGuid()}")
+                .ToReadOnlyReactivePropertySlim(name);
+        }
+    }
+
+    public class ItemViewModel : IDisposable
+    {
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+        public ReadOnlyReactivePropertySlim<string> Name { get; }
+        public ItemViewModel(Item item)
+        {
+            Name = item
+                .Name
+                .ObserveOnUIDispatcher()
+                .ToReadOnlyReactivePropertySlim().AddTo(Disposable);
+        }
+
+        public void Dispose() => Disposable.Dispose();
+    }
+}
---
+++ ComboBoxSample/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace ComboBoxSample
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private ObservableCollection<Item> Source { get; }
+        public ReadOnlyReactiveCollection<ItemViewModel> Items { get; }
+
+        public ReactivePropertySlim<ItemViewModel> SelectedItem { get; }
+
+        public MainWindowViewModel()
+        {
+            Source = new ObservableCollection<Item>(new[] { "Tanaka", "Kimura", "Sakata", "Inoue" }
+                .Select(x => new Item(x)));
+            Items = Source.ToReadOnlyReactiveCollection(x => new ItemViewModel(x));
+            SelectedItem = new ReactivePropertySlim<ItemViewModel>(Items.First());
+        }
+    }
+}
